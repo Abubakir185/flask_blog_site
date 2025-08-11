852b98a5eb84cc498db0c1ef39e022d6c082e952
Abubakir Bohodirov <abubakrbohodirov185@gmail.com>
2025-08-11 13:02:55 +0500

    done

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ db = SQLAlchemy()
 
 KEY = os.getenv("SECRET_KEY")
 
+
 app = Flask(__name__)
 moment = Moment(app)
 app.config['SECRET_KEY'] = KEY
@@ -19,6 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
+from app import *
 from models import User, Post, Comment, Category
 
 login_manager = LoginManager(app)
