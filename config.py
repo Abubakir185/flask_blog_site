f43f9b9cfec04f27efad29576da20110fd756495
Abubakir Bohodirov <abubakrbohodirov185@gmail.com>
2025-08-11 12:21:55 +0500

    done

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
 
+KEY = os.getenv("SECRET_KEY")
+
 app = Flask(__name__)
 moment = Moment(app)
-app.config['SECRET_KEY'] = '8A76b56f8He34M4a88debQ582ea187b6'
+app.config['SECRET_KEY'] = KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
