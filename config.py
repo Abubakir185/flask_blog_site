0d660c055809bd3db8ab1ccdffe5eff5138da439
Abubakir Bohodirov <abubakrbohodirov185@gmail.com>
2025-08-31 15:15:04 +0500

    done

--- config.py
+++ config.py
@@ -6,16 +6,21 @@ from flask_wtf import CSRFProtect
 from flask_moment import Moment
 from dotenv import load_dotenv
 
+# .env fayldan o‘qish
 load_dotenv()
 
+# Flask extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
 csrf = CSRFProtect()
 moment = Moment()
 
+# Flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+# PostgreSQL ulanish (DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/dbname")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Extensions init
@@ -24,4 +29,5 @@ login_manager.init_app(app)
 csrf.init_app(app)
 moment.init_app(app)
 
+# Login uchun redirect qilingan sahifa
 login_manager.login_view = 'login'
